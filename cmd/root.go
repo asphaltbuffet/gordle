8c4d87f7f5b1e73b96bbb9079075ef6813346354
Ben Lechlitner <otherland@gmail.com>
2022-03-03 21:17:00 -0500

    setup basic command

---
+++ cmd/root.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2021 Ben Lechlitner <otherland@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+// Package cmd contains all CLI commands used by the application.
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+const rootCommandLongDesc = "Gordle is a wordle helper application that essentially plays the game for you."
+
+// rootCmd represents the base command when called without any subcommands.
+var rootCmd = &cobra.Command{
+	Use:               "gordle",
+	Version:           "0.0.1",
+	Short:             "Gordle is a wordle helper application",
+	Long:              rootCommandLongDesc,
+	CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Println(GetFirstWord())
+	},
+}
+
+func init() {
+}
+
+// GetFirstWord returns the best starting word based on current word list.
+func GetFirstWord() string {
+	return "RATES"
+}
+
+// GetRootCmd gets the application root command.
+func GetRootCmd() *cobra.Command {
+	return rootCmd
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
